1df9c8cf6617c2b33ca15ae432fa94248f33cccc
Joseph Braide <braidej@gmail.com>
2022-01-21 11:21:26 +0100

    created url and view for CreateShortURL logic

--- urlshortner/api/views.py
+++ urlshortner/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# rest_framework's APIView
+from rest_framework.view import APIView
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+# import response
+from rest_framework.response import Response
+
+# import model
+from .models import URLShortner
+
+# import serializer
+from .serializers import CreateShortURLSerializer
+
+class CreateShortURL(APIView):
+    serializer_class = CreateShortURLSerializer
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.serializer_class()
+        return Response(serializer.data)
+
