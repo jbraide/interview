436c3490383d74633897bbf46b5830fb491ea486
Joseph Braide <braidej@gmail.com>
2022-01-21 08:12:09 +0100

    created model

--- urlshortner/api/models.py
+++ urlshortner/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class URLShortner(models.Model):
+    '''
+        Url shortner models Definition
+        * created_date_time: when the shortened url was created
+        * times_followed: to get the number of times the url was followed
+        * original_url: the inputed link by the user (i.e the original link)
+        * short_url: the shortened url
+    '''
+    created_date_time = models.DateTimeField()
+    times_followed = models.PositiveInteger(default=0)
+    original_url = models.URLFIELD()
+    short_url = models.CharField(max_length=15, unique=True, blank=True)
+
+
+    class Meta:
+        ordering = ['-created']
+
+
+    def __str__(self):
+        return f'shortened {self.original_url} to {self.short_url}'
